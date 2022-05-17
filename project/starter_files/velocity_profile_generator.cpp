1f59e35a3e8d078b1b0ff2dd45c082f8fe2db8ac
Atanasko Boris Mitrev <atanasko.mitrev@gmail.com>
2022-05-18 01:43:34 +0200

    Upate velocity profile generator

--- project/starter_files/velocity_profile_generator.cpp
+++ project/starter_files/velocity_profile_generator.cpp
@@ -354,6 +354,7 @@ double VelocityProfileGenerator::calc_distance(const double& v_i,
                                                const double& v_f,
                                                const double& a) const {
   double d{0.0};
+
   if (std::abs(a) < DBL_EPSILON) {
     d = std::numeric_limits<double>::infinity();
   } else {
@@ -362,8 +363,9 @@ double VelocityProfileGenerator::calc_distance(const double& v_i,
     // v_i (initial velocity) to v_f (final velocity) at a constant
     // acceleration/deceleration "a". HINT look at the description of this
     // function. Make sure you handle div by 0
-    d = 0;  // <- Update
+    d = std::abs((std::pow(v_f, 2) - std::pow(v_i, 2)) / (2 * a));
   }
+
   return d;
 }
 
@@ -385,7 +387,8 @@ double VelocityProfileGenerator::calc_final_speed(const double& v_i,
   // and make v_f = 0 in that case. If the discriminant is inf or nan return
   // infinity
 
-  double disc = 0;  // <- Fix this
+  double disc = std::pow(v_i, 2) + 2 * a * d;
+
   if (disc <= 0.0) {
     v_f = 0.0;
   } else if (disc == std::numeric_limits<double>::infinity() ||
